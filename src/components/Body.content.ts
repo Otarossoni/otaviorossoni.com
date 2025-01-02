663c5993b14a3159f7d8f5f2f3a3cbdd1edf6a0e
Otarossoni <otarossoni@gmail.com>
2025-01-01 22:50:37 -0300

    feat: adiciona Luiza Labs nas experiências

--- src/components/Body.content.ts
+++ src/components/Body.content.ts
@@ -5,7 +5,9 @@ fmt.Println("Bem-vindo, forasteiro!")
 
 ## Experiências
 
-- <a href="https://www.sbsistemas.com.br/" target="_blank">SB Sistemas</a> <time>(04/2024 - Atualmente)</time>: Como desenvolvedor back-end Go, meu objetivo é colaborar com o crescimento dos produtos e soluções oferecidas pela empresa, garantindo entregas que tragam valor aos clientes.
+- <a href="https://www.linkedin.com/company/luizalabs/" target="_blank">Luiza Labs</a> <time>(11/2024 - Atualmente)</time>: Como desenvolvedor back-end Go, meu objetivo é colaborar com o crescimento das soluções internas oferecidas pela empresa, garantindo entregas que tragam valor aos usuários, sempre almejando um código íntegro e seguro.
+
+- <a href="https://www.sbsistemas.com.br/" target="_blank">SB Sistemas</a> <time>(04/2024 - 11/2024)</time>: Como desenvolvedor back-end Go, meu objetivo é colaborar com o crescimento dos produtos e soluções oferecidas pela empresa, garantindo entregas que tragam valor aos clientes.
 
 - <a href="https://www.dataintegra.com.br/" target="_blank">DataIntegra</a> <time>(08/2022 - 04/2024)</time>: Como desenvolvedor back-end Node.js, meu objetivo era colaborar com o crescimento da empresa e suas áreas de atuação por meio de integrações voltadas à área da saúde.
 
