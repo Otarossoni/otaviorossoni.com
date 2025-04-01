26816ea787b9dd2a18706360fa3ac63e3be9b914
Otarossoni <otarossoni@gmail.com>
2025-04-01 06:58:25 -0300

    feat: modifica cores e compacta conteúdo da página

--- src/components/Body.content.ts
+++ src/components/Body.content.ts
@@ -1,11 +1,7 @@
 export const bodyContent = `
-\`\`\`go
-fmt.Println("Bem-vindo, forasteiro!")
-\`\`\`
-
 ## Experiências
 
-- <a href="https://www.linkedin.com/company/luizalabs/" target="_blank">Luiza Labs</a> <time>(11/2024 - Atualmente)</time>: Como desenvolvedor back-end Go, meu objetivo é colaborar com o crescimento das soluções internas oferecidas pela empresa, garantindo entregas que tragam valor aos usuários, sempre almejando um código íntegro e seguro.
+- <a href="https://www.linkedin.com/company/luizalabs/" target="_blank">Luiza Labs</a> <time>(11/2024 - Atualmente)</time>: Como desenvolvedor back-end, meu objetivo é colaborar com o crescimento das soluções internas oferecidas pela empresa, garantindo entregas que tragam valor aos usuários, sempre almejando um código íntegro e seguro.
   - Desenvolvedor Pleno <time>(11/2024 - Atualmente)</time>
 
 - <a href="https://www.sbsistemas.com.br/" target="_blank">SB Sistemas</a> <time>(04/2024 - 11/2024)</time>: Como desenvolvedor back-end Go, meu objetivo é colaborar com o crescimento dos produtos e soluções oferecidas pela empresa, garantindo entregas que tragam valor aos clientes.
@@ -22,23 +18,15 @@ fmt.Println("Bem-vindo, forasteiro!")
 
 Os projetos são importantes para exercitar todo o conhecimento adquirido por meio de cursos e livros. Abaixo, cito os que realizei e elaboro um pouco sobre cada um deles:
 
-- 📖 <a href="https://github.com/Otarossoni/projeto-integrador-ads" target="_blank">Integrador ADS - UPF</a>: Meu trabalho de conclusão de curso em Análise e Desenvolvimento de Sistemas, na <a href="https://www.upf.br/" target="_blank">UPF (Universidade de Passo Fundo)</a>. Com tema livre, escolhi desenvolver uma plataforma de compartilhamento de promoções de lojas digitais, utilizando a stack <a href="https://www.mongodb.com" target="_blank">MongoDB</a>, <a href="https://nodejs.org/en" target="_blank">Node.js</a> e <a href="https://react.dev" target="_blank">React</a>. Originalmente, esse não seria meu projeto, mas decidi mudar a ideia principal faltando apenas dois meses para a entrega. Com foco total, consegui a nota máxima na disciplina através deste projeto.
+- <a href="https://github.com/Otarossoni/projeto-integrador-ads" target="_blank">Integrador ADS - UPF</a>: Meu trabalho de conclusão de curso em Análise e Desenvolvimento de Sistemas, na <a href="https://www.upf.br/" target="_blank">UPF (Universidade de Passo Fundo)</a>. Com tema livre, escolhi desenvolver uma plataforma de compartilhamento de promoções de lojas digitais, utilizando a stack <a href="https://www.mongodb.com" target="_blank">MongoDB</a>, <a href="https://nodejs.org/en" target="_blank">Node.js</a> e <a href="https://react.dev" target="_blank">React</a>. Originalmente, esse não seria meu projeto, mas decidi mudar a ideia principal faltando apenas dois meses para a entrega. Com foco total, consegui a nota máxima na disciplina através deste projeto.
 
-- 📖 <a href="https://github.com/Otarossoni/upf-oficina-cleannode" target="_blank">Cleannode</a>: Alguns meses após minha formatura, recebi a oportunidade de apresentar uma oficina de tema livre na universidade em que me formei. Decidi unir meu conhecimento em <a href="https://www.typescriptlang.org/" target="_blank">TypeScript</a> e meus estudos sobre <a href="https://www.amazon.com.br/C%C3%B3digo-limpo-Robert-C-Martin/dp/8576082675" target="_blank">Clean Code</a> nessa oficina, que tive 4 horas para apresentar formas de implementar o código limpo em <a href="https://www.typescriptlang.org/" target="_blank">TypeScript</a> para uma turma de estudantes de cursos de TI da Universidade.
+- <a href="https://github.com/Otarossoni/upf-oficina-cleannode" target="_blank">Cleannode</a>: Alguns meses após minha formatura, recebi a oportunidade de apresentar uma oficina de tema livre na universidade em que me formei. Decidi unir meu conhecimento em <a href="https://www.typescriptlang.org/" target="_blank">TypeScript</a> e meus estudos sobre <a href="https://www.amazon.com.br/C%C3%B3digo-limpo-Robert-C-Martin/dp/8576082675" target="_blank">Clean Code</a> nessa oficina, que tive 4 horas para apresentar formas de implementar o código limpo em <a href="https://www.typescriptlang.org/" target="_blank">TypeScript</a> para uma turma de estudantes de cursos de TI da Universidade.
 
-- 📃 <a href="https://github.com/Otarossoni/otaviorossoni.com" target="_blank">Site Pessoal</a>: Um site pessoal com minhas informações de contato, conhecimentos, tecnologias que estou estudando e projetos concluídos.
+- <a href="https://github.com/Otarossoni/otaviorossoni.com" target="_blank">Site Pessoal</a>: Um site pessoal com minhas informações de contato, conhecimentos, tecnologias que estou estudando e projetos concluídos.
 
-- 🐿️ <a href="https://github.com/Otarossoni/vscode-go-snippets" target="_blank">VS Code Golang Snippets</a>: Uma simples extensão para o <a href="https://code.visualstudio.com/" target="_blank">Visual Studio Code</a> que adiciona alguns snippets para a linguagem de programação <a href="https://go.dev/" target="_blank">Go</a>.
+- <a href="https://github.com/Otarossoni/desafio-luiza-labs" target="_blank">Desafio Técnico Luiza Labs</a>: Desafio técnico do processo seletivo do <a href="https://www.linkedin.com/company/luizalabs/" target="_blank">Luiza Labs</a>. Trata-se de um serviço de consulta de CEP, cujo objetivo é permitir que o usuário obtenha seu endereço a partir do CEP. 
 
-- 🔷 <a href="https://github.com/Otarossoni/desafio-luiza-labs" target="_blank">Desafio Técnico Luiza Labs</a>: Desafio técnico do processo seletivo do <a href="https://www.linkedin.com/company/luizalabs/" target="_blank">Luiza Labs</a>. Trata-se de um serviço de consulta de CEP, cujo objetivo é permitir que o usuário obtenha seu endereço a partir do CEP. 
+- <a href="https://github.com/Otarossoni/data-structures-and-algorithms.lang" target="_blank">DSA.lang</a>: Algoritmos e estruturas de dados conhecidos são as pedras de amolar do pensamento lógico de qualquer desenvolvedor. Neste repositório, organizo os que estou estudando e faço anotações sobre eles.
 
-- 📔 <a href="https://github.com/Otarossoni/mangadex-downloader" target="_blank">Mangadex Downloader</a>: Uma CLI escrita em <a href="https://go.dev/" target="_blank">Go</a>, cujo objetivo é comunicar com a API da plataforma [Mangadex](https://mangadex.org/) e disponibilizar as imagens dos capítulos de mangá em alta definição, compactados em arquivos .zip e .cbz prontos para leitura, entre outras funcionalidades.
-
----
-
-## Estudos
-
-- ♾️ <a href="https://github.com/Otarossoni/data-structures-and-algorithms.lang" target="_blank">DSA.lang</a>: Algoritmos e estruturas de dados conhecidos são as pedras de amolar do pensamento lógico de qualquer desenvolvedor. Neste repositório, organizo os que estou estudando e faço anotações sobre eles.
-
-- 🛠️ <a href="https://github.com/Otarossoni/workshop" target="_blank">Workshop</a>: Um repositório voltado para pequenos desafios e experimentos em tecnologias que não conheço ou estou aprendendo.
+- <a href="https://github.com/Otarossoni/mangadex-downloader" target="_blank">Mangadex Downloader</a>: Uma CLI escrita em <a href="https://go.dev/" target="_blank">Go</a>, cujo objetivo é comunicar com a API da plataforma [Mangadex](https://mangadex.org/) e disponibilizar as imagens dos capítulos de mangá em alta definição, compactados em arquivos .zip e .cbz prontos para leitura, entre outras funcionalidades.
 `;
--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,16 +1,11 @@
-import { At, GithubLogo, InstagramLogo, LinkedinLogo, ReadCvLogo, XLogo } from "@phosphor-icons/react"
+import { Envelope, GithubLogo, InstagramLogo, LinkedinLogo, XLogo } from "@phosphor-icons/react"
 
 import { useEffect, useRef } from 'react'
 import { marked } from 'marked'
-import Prism from 'prismjs'
 
 import { bodyContent } from './Body.content'
-import cvPdf from '../assets/cv.pdf'
 
 import styles from './Body.module.css'
-import 'prismjs/components/prism-typescript'
-import 'prismjs/components/prism-go'
-import 'prismjs/themes/prism-okaidia.css'
 
 function Body() {
   const contentRef = useRef<HTMLDivElement>(null)
@@ -18,23 +13,12 @@ function Body() {
   const iconColor = "#FFF"
   const iconStyle = "light"
 
-  const handleDownloadCV = () => {
-    const link = document.createElement('a')
-    link.href = cvPdf
-    link.download = 'CV - Otávio Monteiro Rossoni.pdf'
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
-  }
-
-
   useEffect(() => {
     const fetchContainerContent = async () => {
       const readmeHtml = await marked(bodyContent)
 
       if (contentRef.current) {
         contentRef.current.innerHTML = readmeHtml
-        Prism.highlightAllUnder(contentRef.current)
       }
     }
 
@@ -45,10 +29,7 @@ function Body() {
     <>
       <div className={styles.header}>
         <a title="E-mail" href="mailto:otarossoni@gmail.com">
-          <At color={iconColor} size={iconSize} weight={iconStyle} />
-        </a>
-        <a title="CV" onClick={handleDownloadCV} target="_blank">
-          <ReadCvLogo color={iconColor} size={iconSize} weight={iconStyle} />
+          <Envelope color={iconColor} size={iconSize} weight={iconStyle} />
         </a>
         <a title="GitHub" href="https://github.com/Otarossoni" target="_blank">
           <GithubLogo color={iconColor} size={iconSize} weight={iconStyle} />
